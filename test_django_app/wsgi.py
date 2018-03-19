2df21d7fa325f7611417c2f06e211aa5e2a4abb9
Saurav <sp.gharti@gmail.com>
2018-03-20 02:52:06 +0545

    mf

--- test_django_app/wsgi.py
+++ test_django_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sgmagar_crowdbotics_17.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_django_app.settings")
 
 application = get_wsgi_application()
